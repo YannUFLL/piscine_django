fce78cfb024da1321b4f05fc86b942481742ba89
YannUFLL <yannlescoul79@free.fr>
2025-12-11 16:15:09 +0100

    fix(day03): add file reading in my_program

--- day03/ex01/my_program.py
+++ day03/ex01/my_program.py
@@ -7,6 +7,8 @@ def execute():
     p.makedirs_p()
     file = p / "my_loved_file"
     file.write_text("\u266CCOUCOU_LES_CONGOLAIS\u266C")
+    file_content = file.read_text()
+    print(file_content)
 
 if __name__ == "__main__":
     execute()
