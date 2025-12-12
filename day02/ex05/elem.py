cfa9508275dbb12927d1d90de0b7b55d51986894
YannUFLL <yannlescoul79@free.fr>
2025-12-12 14:59:34 +0100

    add(day02): required files

--- day02/ex05/elem.py
+++ day02/ex05/elem.py
@@ -103,101 +103,19 @@ class Elem:
                 (type(content) == list and all([type(elem) == Text or
                                                 isinstance(elem, Elem)
                                                 for elem in content])))
-
-class Html(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("html", tag_type="double", content=content, **kwargs)
-
-class Head(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("head", tag_type="double", content=content, **kwargs)
-
-class Body(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("body", tag_type="double", content=content, **kwargs)
-
-class Title(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("title", tag_type="double", content=content, **kwargs)
-
-class Meta(Elem):
-    def __init__(self, **kwargs):
-        super().__init__("meta", tag_type="simple", **kwargs)
-
-class Img(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("img", tag_type="simple", content=content, **kwargs)
-
-class Table(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("table", tag_type="double", content=content, **kwargs)
-
-class Th(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("th", tag_type="double", content=content, **kwargs)
-
-class Tr(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("tr", tag_type="double", content=content, **kwargs)
-
-class Td(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("td", tag_type="double", content=content, **kwargs)
-
-class Ul(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("ul", tag_type="double", content=content, **kwargs)
-
-class Ol(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("ol", tag_type="double", content=content, **kwargs)
-
-class Li(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("li", tag_type="double", content=content, **kwargs)
-
-class H1(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("h1", tag_type="double", content=content, **kwargs)
-
-class H2(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("h2", tag_type="double", content=content, **kwargs)
-
-class P(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("p", tag_type="double", content=content, **kwargs)
-
-class Div(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("div", tag_type="double", content=content, **kwargs)
-
-class Span(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("span", tag_type="double", content=content, **kwargs)
-
-class Hr(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("hr", tag_type="simple", content=content, **kwargs)
-
-class Br(Elem):
-    def __init__(self, content=None, **kwargs):
-        super().__init__("br", tag_type="simple", content=content, **kwargs)
-
 if __name__ == '__main__':
     print(
-        Html([
-            Head(
-                Title(
+        Elem("html",tag_type="double", content=[
+            Elem("head", tag_type="double", content=
+                Elem("title",tag_type="double", content=
                     Text('"Hello ground!"'))),
-                Body([
-                    H1(
+                Elem("body", tag_type="double", content=[
+                    Elem("h1", tag_type="double", content=
                         Text('"Oh no, not again!"')),
-                    Img(attr={"src":"http://i.imgur.com/pfp3T.jpg"})])]))
+                    Elem("img", tag_type="simple", attr={"src":"http://i.imgur.com/pfp3T.jpg"})])]))
     print(Text(">"))
-    print(Div())
+    print(Elem())
     try:
-        Html(1)
+        Elem(content=1)
     except Exception as e: 
         print(type(e) == Elem.ValidationError)
-    print("hello")
