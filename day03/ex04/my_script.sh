743ed2eb17bcc36dd3dc76803f7c162b73c69f57
YannUFLL <yannlescoul79@free.fr>
2026-01-05 15:36:01 +0100

    fix: no prefix in venv shell prompt

--- day03/ex04/my_script.sh
+++ day03/ex04/my_script.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
 set -e
+
+rm -rf django_venv
+
 python3 -m venv django_venv
-. django_venv/bin/activate
-pip install -r requirement.txt
-exec $SHELL
+
+./django_venv/bin/pip install -r requirement.txt
+
+echo "Environment is ready. Switching to interactive mode..."
+
+exec bash --rcfile <(echo "source ~/.bashrc; source django_venv/bin/activate"
