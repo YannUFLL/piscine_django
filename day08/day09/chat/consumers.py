9914e79826b4f286a1d6c370e039e8ee898e5c68
YannUFLL <yannlescoul79@free.fr>
2026-01-11 00:08:18 +0100

    add(day08): channels, xss protection, better appearance

--- day08/day09/chat/consumers.py
+++ day08/day09/chat/consumers.py
@@ -3,12 +3,20 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+from .models import Room
+
 chat_history = {}  
 connected_users = {}
 
 class chatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_group_name = 'test'
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+    
+        if not Room.objects.filter(name=self.room_name).exists():
+            self.close()
+            return
+
+        self.room_group_name = f'chat_{self.room_name}'
 
         user = self.scope["user"]
         if user.is_authenticated:
@@ -83,6 +91,12 @@ class chatConsumer(WebsocketConsumer):
         {
             "type":"user_leave",
             "username":self.scope["user"].username
+        })
+        async_to_sync(self.channel_layer.group_send)(self.room_group_name,
+        {
+            "type":"server_message",
+            "username":self.scope['user'].username,
+            "message": f"{self.scope['user'].username} has left the chat"
         }
         )
         async_to_sync(self.channel_layer.group_discard)(
--- day08/day09/chat/views.py
+++ day08/day09/chat/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Room
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def lobby(request):
-    return render(request, "chat/lobby.html")
+    rooms = Room.objects.all() 
+    return render(request, "chat/lobby.html", {'rooms': rooms})
+
+@login_required(login_url='/account/')  
+def room(request, room_name):
+    get_object_or_404(Room, name=room_name)
+    return render(request, "chat/room.html", {'room_name': room_name})
